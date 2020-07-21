e85fd9f68e314001eeb23cff547a4f4ffeb032a2
ArtzRetter <samuel.cm@outlook.com>
2020-07-21 15:47:42 -0300

    primeira aula de javascript

---
+++ Js/main.js
@@ -0,0 +1,63 @@
+function funcaoChange(elemento){console.log(elemento.value)}
+
+/* existe além do window.open o window.location.href que abre na mesma pagina em que você está */
+function redirec(){window.open("https://senninha.com.br/")}
+
+//function clique(){document.getElementById("pinto").innerHTML = ".i.";}
+//function clique(){alert(".i.")}
+
+/* cuidado com var global e local */
+
+//function soma(n1, n2){return n1 + n2;}
+
+//function validaIdade(idade){
+//    var validar; if (idade >= 18){validar = true}
+//    else
+//    {validar = false }return validar}
+//var idade = prompt("qual a sua idade?")
+//console.log(validaIdade(idade));
+
+//var data = new Date();
+//alert(data.getDay());                         
+/* existem getDay getMonth getHour.... */
+
+//var count = 0;
+//for(count+0; count<= 6; count++){ alert(count)};
+
+//var count = 0;
+//while (count <= 10){console.log(count);
+//count = count + 1;                /* também pode ser count++ */           
+//alert(count);}     
+
+/* usar prompt e >< */
+//var idade = prompt("qual a sua idade?");
+//if (idade >= 18){alert("maior de idade")}else{alert("menor de idade")};
+
+/* lista + dicionário */
+//var mobs = [{name:"zombie", drop:"rotten flesh"}, {name:"skeleton", drop:"bone"}, {name:"spider", drop:"web"}];
+//console.log(mobs);
+//alert(mobs[1].drop);
+
+/* dicionario */
+//var mob = {name:"zombie", drop:"rotten flesh"};
+//console.log(mob.name);
+//alert(mob.drop);
+
+//var list = ["terra", "madeira", "folhas"];
+//list.pop(0);                      /* para remover itens de uma lista */
+//list.push("frutos")               /* adicionar itens a lista */
+//console.log(list.length);         /* quantidade de itens na lista */
+//console.log(list.reverse);        /* reverter a ordem da lista */
+//console.log(list.ToString);       /* "impressão" do item da lista */
+//console.log(list.join(" - "));    /* separar por - não por , */
+
+//var frase = "tira o dedo do cu"
+//console.log(frase.replace ("cu", "nariz"));
+//alert(frase.replace("cu", "nariz"));
+//var idade = 17;
+//var idade2 = 1;
+//alert(idade + idade2);
+//var nome = "seu nome"
+//alert("e o cu, já deu?" + nome);
+
+/* ToUperCase & TolowerCase */
